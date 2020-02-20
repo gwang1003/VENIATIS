a678b711e8e20920c1532d4b0e37151ed904f666
user2 <user2@user2-PC>
2020-02-20 20:52:26 +0900

    혜림0220

--- Veniatis01/src/main/java/com/kh/veniatis/project/user/model/service/ProjectUserService.java
+++ Veniatis01/src/main/java/com/kh/veniatis/project/user/model/service/ProjectUserService.java
@@ -2,8 +2,8 @@ package com.kh.veniatis.project.user.model.service;
 
 import java.util.ArrayList;
 
+import com.kh.veniatis.common.files.model.vo.Files;
 import com.kh.veniatis.member.model.vo.Member;
-import com.kh.veniatis.project.creator.model.vo.Project;
 import com.kh.veniatis.project.creator.model.vo.Reward;
 import com.kh.veniatis.project.user.model.vo.ProjectView;
 
@@ -22,6 +22,15 @@ public interface ProjectUserService {
 	//int selectCreatorNumber(int creNo);
 	//Member selectCreatorInfo(int mNo);
 	Member selectCreatorInfo(int creNo);
+
+	// 프로젝트 사진 파일 가져오기
+	ArrayList<Files> selectFileList(int pNo);
+
+	// 프로젝트 qna 댓글 목록 가져오기
+	//ArrayList<Reply> selectReplyList(int pNo);
+
+	// 프로젝트 qna 댓글 등록
+	//int insertReply(Reply r);
 
 	// 최근 소식 가져오기
 	
